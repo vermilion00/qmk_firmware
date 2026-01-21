127785349173c2d97ea83ee6dc966ef97cfa63bf
vermilion00 <lucaspengler0@gmail.com>
2026-01-21 16:13:20 +0100

    Added keycodes to change profile, start calibration, print calibration data

--- quantum/process_keycode/process_analog_matrix.h
+++ quantum/process_keycode/process_analog_matrix.h
@@ -6,7 +6,3 @@ bool process_analog_matrix(uint16_t keycode, keyrecord_t *record);
 
 void print_calibration_data(void);
 
-uint8_t get_active_profile(void);
-
-void set_active_profile(uint8_t profile);
-
